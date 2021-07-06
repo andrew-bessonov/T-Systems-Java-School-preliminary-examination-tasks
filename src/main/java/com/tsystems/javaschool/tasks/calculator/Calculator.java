128f8d57745bc824392e0d1e8997639fe8298880
bessik <bessik2108@gmail.com>
2021-07-07 01:49:28 +0300

    (Check_other_branch)here_uses_library

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package com.tsystems.javaschool.tasks.calculator;
 
+import org.mariuszgromada.math.mxparser.Expression;
+
 public class Calculator {
 
     /**
@@ -11,8 +13,29 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
-        return "";
+        if (!isValid(statement)) {
+            return null;
+        }
+
+        Expression e = new Expression(statement);
+        double v = e.calculate();
+
+        if (!isValid(String.valueOf(v))) {
+            return null;
+        }
+
+        if (v % 1 == 0) {
+            return String.valueOf(v).replace(".0", "");
+        }
+
+        return String.valueOf(v);
     }
 
+    private boolean isValid(String statement) {
+        if (statement == null || statement.equals("")) {
+            return false;
+        }
+        return statement.matches("^[\\d\\+\\/\\*\\.\\- \\(\\)]*$");
+    }
 }
+
