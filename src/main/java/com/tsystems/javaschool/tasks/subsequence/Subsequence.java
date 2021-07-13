b336d1e17537838f5c92048220d4889c3726562d
bessik <bessik2108@gmail.com>
2021-07-13 03:51:15 +0300

    Subsequence test ready

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,6 +1,8 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Subsequence {
 
@@ -13,8 +15,37 @@ public class Subsequence {
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
     @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
+    public boolean find(List<?> x, List<?> y) {
+
+        if(x == null || y == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if(x.size() > y.size()) {
+            return false;
+        }
+
+        if(x.isEmpty()) {
+            return true;
+        }
+
+        Queue yQueue = new LinkedList<>(y);
+        Queue xQueue = new LinkedList<>(x);
+
+        Object xItem = xQueue.remove();
+        Object yItem;
+
+        while(!yQueue.isEmpty()) {
+            yItem = yQueue.remove();
+
+            if(yItem.equals(xItem)) {
+                if(xQueue.isEmpty()) {
+                    return true;
+                }
+                xItem = xQueue.remove();
+            }
+        }
+
         return false;
     }
 }
